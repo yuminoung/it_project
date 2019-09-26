048d9a920ac267d4f5d4b77d94f47370cd78d852
andy-niu-uom <zirun@student.unimelb.edu.au>
2019-09-26 17:10:52 +1000

    hello

--- lib/pages/all_pages.dart
+++ lib/pages/all_pages.dart
@@ -6,9 +6,6 @@ export 'bottom/bottom_me.dart';
 
 export 'auth/login.dart';
 export 'auth/register.dart';
-<<<<<<< HEAD
-=======
 export 'edit_post.dart';
 
->>>>>>> ac7d9cf513e14df3a2f1d14315367e4ddf7a8e4c
 export 'landing.dart';
