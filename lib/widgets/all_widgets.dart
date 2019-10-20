fa729e919b068aad4460384c504c51df103844db
oliver8606 <oliver8606@gmail.com>
2019-10-20 14:13:51 +1100

    added gallery, user can see photos in the family group some minor update on home.dart added a custom widget arrow back

--- lib/widgets/all_widgets.dart
+++ lib/widgets/all_widgets.dart
@@ -6,3 +6,4 @@ export 'custom_slide_from_bottom_page_route_builder.dart';
 export 'custom_divider.dart';
 export 'custom_progress_indicator.dart';
 export 'custom_pop_button.dart';
+export 'custom_arrow_back.dart';
